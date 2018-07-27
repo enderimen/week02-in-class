14f41c2953b09ead5d516ac9d40f413fd2ed9918
Acer <enderimen@hotmail.com>
2018-07-28 00:38:21 +0300

    initial commit

---
+++ size.scala
@@ -0,0 +1,16 @@
+var list = Array(1, 9, 4, 3);
+
+def size(list: List[Int]): Int = {
+     
+	 var i = 0;
+
+      while(i < (list.length + 1)){
+
+     	i += 1;
+    	
+	 }
+     
+	 i
+}
+
+print("Length of Array: " + size(list));
